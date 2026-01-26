2c92b2df109cc138180f454be083162f0577059e
Radosław WIeczorek <r.p.wieczorek@gmail.com>
2026-01-26 10:03:39 +0100

    Fix js according to the guideline

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
-const swiper = new Swiper('.es-swiper1', {
-  direction: 'horizontal',
-  loop: false,
-  autoHeight: true,
+(() => {
+  const swiper = new Swiper('#es-rc #es-garnier .es-swiper1', {
+    direction: 'horizontal',
+    loop: false,
+    autoHeight: true,
 
-  pagination: {
-    el: '.es-swiper1-pagination',
-  },
+    pagination: {
+      el: '#es-rc #es-garnier .es-swiper1-pagination',
+    },
 
-  navigation: {
-    nextEl: '.es-swiper1-button-next',
-    prevEl: '.es-swiper1-button-prev',
-  },
+    navigation: {
+      nextEl: '#es-rc #es-garnier .es-swiper1-button-next',
+      prevEl: '#es-rc #es-garnier .es-swiper1-button-prev',
+    },
 
-});
+  })
+})()
